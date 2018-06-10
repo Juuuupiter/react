e37fe9b8d63048d6035d5383bbcddb95c394d704
Juuuupiter <691107824@qq.com>
2018-06-10 14:26:46 +0800

    react demo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
-import App from './App';
-// import demo from './demo';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
